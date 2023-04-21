a695a739d6cde9db6fb6aa9b22b2565375f63055
vanessa garcia <venessag0908@gmail.com>
2023-04-21 13:30:12 -0600

    to-do list mini project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  addingEventListeners()
 });
+
+function addingEventListeners(){
+  document
+    .getElementById("create-task-form")
+    .addEventListener("submit", handleFormSubmit)
+}
+
+function handleFormSubmit(event){
+  event.preventDefault()
+  //console.log(event)
+  const task = event.target["new-task-description"].value
+  //console.log(task)
+  displayTask(task)
+}
+
+function displayTask(task){
+  const ulTask = document.getElementById("tasks")
+  //console.log(ulTask)
+  const liTask = document.createElement("li")
+  liTask.textContent = task
+  //console.log(liTask)
+  ulTask.appendChild(liTask)
+}
